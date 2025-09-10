c236d23d5a21fbbf3e0d42f4a990e2a49218b2fc
JelloJanitor <theboscomullinax@gmail.com>
2025-09-10 15:57:11 -0500

    Added paddles.
    In the current state, there are two paddles that will not rotate and will only translate along the x-axis.
    
    Player input does not exist, and ball-paddle interactions still require attention.

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int playerId;
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float axisValue = GetAxisValue();
+        MovePaddle(axisValue);
+    }
+
+    private void MovePaddle(float axisValue)
+    {
+        Vector2 velocity = rb.linearVelocity;
+        velocity.y = axisValue * moveSpeed;
+        rb.linearVelocity = velocity;
+    }
+
+    private float GetAxisValue()
+    {
+        float axisValue = 0f;
+
+        if (playerId == 1) axisValue = Input.GetAxis("PaddlePlayer1");
+        else if (playerId == 2) axisValue = Input.GetAxis("PaddlePlayer2");
+
+        return axisValue;
+    }
+}
